4e561e5cdf8ca0574410bf7a225b094e88ca35ca
John Doe <omurbek.aksaibek@gmail.com>
2025-03-14 17:02:22 +0100

    test

---
+++ src/TestTapwyrma.java
@@ -0,0 +1,8 @@
+public class TestTapwyrma {
+    public static void main(String[] args){
+        int tabbliza = 6;
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(tabbliza + " * " + j + " = " + (tabbliza * j));
+        }
+    }
+}
