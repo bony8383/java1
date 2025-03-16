324a65b20ece211f0dbc97f5407ccb09321044ef
Азамат Токтакунов <azamat@toktakunov.com>
2025-03-16 22:45:00 +0100

    reinit

--- src/OmkaTask1.java
+++
@@ -1,8 +0,0 @@
-public class OmkaTask1 {
-    public static void main(String[] args) {
-        System.out.println("Сизге кайсы сандын көбөйтүү таблицасы керек?: ");
-       for (int i = 10; i >=1; i--) {
-           System.out.println("Tetiri: " + i);
-       }
-    }
-}
