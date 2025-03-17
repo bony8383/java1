ab964376a4ec861ed6145282595bf044cf41e085
Азамат Токтакунов <azamat@toktakunov.com>
2025-03-17 14:55:29 +0100

    Hello World

---
+++ src/helloworld/azamat/HelloWorld.java
@@ -0,0 +1,8 @@
+package helloworld.azamat;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Azamat";
+        System.out.println("Salam, " + name);
+    }
+}
