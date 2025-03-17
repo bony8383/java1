9332795c7c7162bc9cd3ce19632a29d533f10d99
GuliraKalykova <gulirakalykova@gmail.com>
2025-03-17 11:53:02 +0300

    My commit Gulira

--- src/Gulira.java
+++ src/Gulira.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Gulira {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            System.out.println("Сан: " + i);
+        }
+
+        class WhileExample {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                String input = "";
+                while (input.equals("exit")) {
+                    System.out.println("Чыгыш үчүн 'exit' жаз");
+                    input = scanner.nextLine();
+                    System.out.println("Cиз " + input + " деп жаздыңыз");
+                }
+                System.out.println("Программа аяктады");
+
+            }
+        }
+    }
 }
+
+
+
