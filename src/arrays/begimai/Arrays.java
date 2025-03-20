1c6d336a0edd607306b5fda3d81526476c8bfe91
Begimai <begimai.rusam.on@gmail.com>
2025-03-20 20:56:07 +0100

    5.1. Бир өлчөмдүү массивдер (One-dimensional arrays) #21

---
+++ src/arrays/begimai/Arrays.java
@@ -0,0 +1,17 @@
+package arrays.begimai;
+
+public class Arrays {public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    int[] patient = {45, 67, 78, 98, 34};
+    int komnata = java.util.Arrays.binarySearch(patient, 98);
+    System.out.println("pat zimmer?" + komnata);
+
+    int[] pesonal = {2, 5, 20, 45};
+    int summa = 0;
+    for (int a : pesonal) {
+        summa += a;
+    }
+    System.out.println("Gesamt personal? " + summa);
+
+}
+}
