b570a4e72130c876015194d28867cd388428b9b8
Aizat_Arapbaeva <aizaarapbaeva@gmail.com>
2025-03-21 23:37:09 +0900

    Массивдер боюнча 3 тапшырма

---
+++ src/methods/aizat/MethodTest.java
@@ -0,0 +1,14 @@
+package methods.aizat;
+
+public class MethodTest {
+    public static void main (String[] args){
+        int result = add(10, 20);  // методду чакыруу
+        System.out.println("Жыйынтык: " + result);
+    }
+
+
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+}
+}
