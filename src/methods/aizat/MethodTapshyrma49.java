d51a6443c30f941907f18f64801f4ff9dfff4bed
Aizat_Arapbaeva <aizaarapbaeva@gmail.com>
2025-03-22 22:18:55 +0900

    Массивдер боюнча 3 тапшырма

---
+++ src/methods/aizat/MethodTapshyrma49.java
@@ -0,0 +1,13 @@
+package methods.aizat;
+
+public class MethodTapshyrma49 {
+    public static void main (String[] args){
+       String result = greetUser("Aizat");
+        System.out.println("Салам" + " " + result);
+    }
+
+    public static String greetUser(String name){
+        String abc = name;
+        return abc;
+    }
+}
