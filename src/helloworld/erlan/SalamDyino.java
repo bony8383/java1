ab64a2ecc70b83faf71595ae86c34d9ec9f4e2b8
erlan-kubanychbekov <erlan.kubanychbekov08@gmail.com>
2025-03-23 21:46:25 +0100

    Salam_Dyino

---
+++ src/helloworld/erlan/SalamDyino.java
@@ -0,0 +1,8 @@
+package helloworld.erlan;
+
+public class SalamDyino {
+    public static void main(String[] args) {
+        String name = "Azamat";
+        System.out.println("Salam, " + name);
+    }
+}
