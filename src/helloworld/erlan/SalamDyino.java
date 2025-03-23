5cc406ed9db63d45c79637a8d48e8551d887f24a
Оmurbek Aksaibek <121406213+Omka-Aksaibek@users.noreply.github.com>
2025-03-23 21:54:08 +0100

    Delete src/helloworld/erlan/SalamDyino.java

--- src/helloworld/erlan/SalamDyino.java
+++
@@ -1,8 +0,0 @@
-package helloworld.erlan;
-
-public class SalamDyino {
-    public static void main(String[] args) {
-        String name = "Azamat";
-        System.out.println("Salam, " + name);
-    }
-}
