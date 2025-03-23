65d73edd153aa92100b0b1a26e6e60f6fe916dbd
erlan-kubanychbekov <erlan.kubanychbekov08@gmail.com>
2025-03-23 21:49:36 +0100

    Hello_World

--- src/helloworld/erlan/HelloWorld.java
+++ src/helloworld/erlan/HelloWorld.java
@@ -2,6 +2,7 @@ package helloworld.erlan;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Salam");
+        String name = "Azamat";
+        System.out.println("Salam, " + name);
     }
 }
