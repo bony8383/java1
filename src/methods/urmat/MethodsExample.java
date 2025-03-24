015d530dbfe71621a39acaa03f858414637eef70
Urmatbek Dubanaev <urmatnd@gmail.com>
2025-03-24 13:32:24 +0600

    #49 simple method

--- src/methods/urmat/MethodsExample.java
+++ src/methods/urmat/MethodsExample.java
@@ -4,9 +4,17 @@ public class MethodsExample {
     public static void main(String[] args) {
         int sum = add(10, 20);
         System.out.println(sum);
+        greetUser("Urmat");
+        greetUser("Samat");
+        greetUser("Venera");
     }
+
     public static int add(int a, int b) {
         int sum = a + b;
         return sum;
+    }
+
+    public static void greetUser(String name) {
+        System.out.println("Привет " + name + "!");
     }
 }
