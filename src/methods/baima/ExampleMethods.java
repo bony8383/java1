03ed0ddc15df0d9ec0c0f6b7a0f5616ab5bacf5c
Baima <baima.omorbekova.ch@gmail.com>
2025-03-24 19:23:10 +0600

    Agaidyn jardamy menen Method Calling

--- src/methods/baima/ExampleMethods.java
+++ src/methods/baima/ExampleMethods.java
@@ -1,28 +1,18 @@
 package methods.baima;
 
 public class ExampleMethods {
-    public static int main (int x, int y ) {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 9;
+        int result = max (a, b);
+        System.out.println("Эн чон сан: "+ result);
+    }
+
+    public static int max (int x, int y ) {
         if (x>y) {
             return x;
         } else {
             return y;
         }
     }
-
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 9;
-
-
-        int result = max(a, b);
-        System.out.println("эн чон сан: " + result);
-
-
-    }
-
-    private static int max(int a, int b) {
-        return b;
-    }
-
-
 }
