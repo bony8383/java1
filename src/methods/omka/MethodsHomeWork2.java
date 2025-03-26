504197e84c350e1e7a43c3fab65004b5ad1823dd
John Doe <omurbek.aksaibek@gmail.com>
2025-03-26 17:56:14 +0100

    Methods_Home_Work_2_Omurbek

---
+++ src/methods/omka/MethodsHomeWork2.java
@@ -0,0 +1,10 @@
+package methods.omka;
+public class MethodsHomeWork2 {
+    public static int variableNumbers(int a, int b){
+        return Math.max(a,b);
+    }
+    public static void main(String[] args){
+        int result = variableNumbers(10, 20);
+        System.out.println(result);
+    }
+}
