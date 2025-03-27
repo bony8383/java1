1c00afcea053600f92fdcac725b8a0a21b8f576c
John Doe <omurbek.aksaibek@gmail.com>
2025-03-27 15:58:06 +0100

    OOP_Setter_Getter_Konstruktor

---
+++ src/oop/omka/Main.java
@@ -0,0 +1,16 @@
+package oop.omka;
+
+public class Main {
+    public static void main(String[] args){
+        Car car1 = new Car();
+        car1.setMarka("Toyota");
+        car1.setPreis(5000);
+
+        Car car2 = new Car("BMW", 10000);
+        car1.all();
+        car2.all();
+        // Setter
+        car1.setPreis(7000);
+        System.out.println("Жаңыртылган баасы: " + car1.getPreis() + " доллар");
+    }
+}
