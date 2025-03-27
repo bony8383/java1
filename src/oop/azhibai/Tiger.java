4c77c9ef34bbf56edec0afbdf8b4598a498196b4
Paizidinuulu <Paizidinuulukg@gmail.com>
2025-03-27 17:25:41 +0600

    delete files

---
+++ src/oop/azhibai/Tiger.java
@@ -0,0 +1,15 @@
+package oop.azhibai;
+
+public class Tiger {
+
+        String breed;
+        int weight;
+
+        public void sayGav(){
+            System.out.println(weight + "кг " + breed + " чуркап бара жатат!");
+
+        }
+
+
+    }
+
