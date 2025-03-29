8456018551104c40e9fe878b171a7288e9148a5b
ZhamalTynch97 <zhamal.020997@mail.de>
2025-03-29 02:05:07 +0100

    Инкапсуляция и модификаторы доступа #69

---
+++ src/oop/zhamal/Smartphone.java
@@ -0,0 +1,28 @@
+package oop.zhamal;
+
+public class Smartphone extends Phone {        // extends >> bul murastoo.
+    private boolean hasCamera;
+    private boolean hasInternet;
+
+    public Smartphone(String brand, String model, double price, boolean hasCamera, boolean hasInternet) {
+        super(brand, model, price);        //  bular phondon >> smarthphongo otkon murastar ( super - bul daiyar Variable)
+        this.hasCamera = hasCamera;
+        this.hasInternet = hasInternet;
+    }
+
+    public boolean hasCamera() {
+        return hasCamera;
+    }
+
+    public boolean HasInternet() {
+        return hasInternet;
+    }
+
+    public void printSmartphoneInfo() {
+        super.printInfo();
+        System.out.println(" Has a Kamera ?" + (hasCamera ? " Yes" : " NO"));
+        System.out.println(" Has a Internet ?" + (hasInternet ? " Yes" : " NO"));
+
+    }
+}
+
