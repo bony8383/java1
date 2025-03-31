e7c35c3f58f0df68c291bd9e83ea38ebcde666b7
ZhamalTynch97 <zhamal.020997@mail.de>
2025-03-31 14:32:23 +0200

    oop

---
+++ src/oop/zhamal/DenSooluk.java
@@ -0,0 +1,11 @@
+package oop.zhamal;
+
+public class DenSooluk {
+    public static void main (String []args){
+
+        System.out.println ( "Balanchaeva Balancha");
+        Patient patient = new Patient(0004," baimap", 33,"Mbank",
+                "kyz" + " a jana b"," a ", 45);
+        patient.belgisizKardarJePatient() ;
+    }
+}
