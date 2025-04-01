3f3911a9b073f9fdb99f27f21a44b781c5dcade5
zhanylmunduzova <janylmunduzova2@gmail.com>
2025-04-01 22:23:36 +0200

    Ifelse.java

---
+++ src/ifelse/zhanyl/Ifelse.java
@@ -0,0 +1,19 @@
+package ifelse.zhanyl;
+
+public class Ifelse {
+    public static void Ifelse(String[] args) {
+        int zahl1=-10;
+        int zahl2=8;
+        int a=zahl1 +zahl2 ;
+        if (  a >0) {
+            System .out .println("он сан");
+        }
+        else if(a   <0) {
+            System .out .println("терс сан");
+        }
+        else {
+            System .out .println("барабар");
+        }
+        System .out .println(a ) ;
+    }
+}
