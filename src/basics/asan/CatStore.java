8f15b02cd2af8b7eb56efb92d358e24554c25e1d
mamad <mamadovasan660@gmail.com>
2025-04-04 00:21:33 +0200

    feature-basics-asan JAV1A-15

--- src/basics/asan/CatStore.java
+++ src/basics/asan/CatStore.java
@@ -2,7 +2,7 @@ package basics.asan;
 
 import java.util.Scanner;
 
-public class Main {
+public class CatStore {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Канча мышык: ");
