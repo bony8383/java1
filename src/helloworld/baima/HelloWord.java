525f47afdb3020cbedde84706297e7d92133965c
Baima <baima.omorbekova.ch@gmail.com>
2025-04-03 15:51:36 +0600

    modul1

---
+++ src/helloworld/baima/HelloWord.java
@@ -0,0 +1,9 @@
+package helloworld.baima;
+
+public class HelloWord{
+public static void main(String[] args) {
+        String name = "Baima";
+        //System.out.println("Кутман кун!");
+        System.out.println("Салам, "+name+ "!");
+        }
+}
