f87ccbbd8b583cb94bde3f8cbc51895ce3e98f0f
zhanylmunduzova <janylmunduzova2@gmail.com>
2025-04-04 13:34:16 +0200

    ForWhile

--- src/ifelse/zhanyl/Ifelse.java
+++ src/ifelse/zhanyl/Ifelse.java
@@ -2,18 +2,20 @@ package ifelse.zhanyl;
 
 public class Ifelse {
     public static void main(String[] args) {
-        int zahl1=-10;
-        int zahl2=8;
-        int a=zahl1 +zahl2 ;
-        if (  a >0) {
-            System .out .println("он сан");
+            int zahl1=-10;
+            int zahl2=8;
+            int a=zahl1 +zahl2 ;
+            if (  a >0) {
+                System .out .println("он сан");
+            }
+            else if(a   <0) {
+                System .out .println("терс сан");
+            }
+            else {
+                System .out .println("барабар");
+            }
+            System .out .println(a ) ;
         }
-        else if(a   <0) {
-            System .out .println("терс сан");
-        }
-        else {
-            System .out .println("барабар");
-        }
-        System .out .println(a ) ;
     }
-}
+
+
