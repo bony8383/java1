12a5773b04fea64980ddaed7b56ce7c8f4d4d96a
Азамат Токтакунов <azamat@toktakunov.com>
2025-04-11 15:21:44 +0200

    #JAVA1B-28: Local API - Azamat - Weather Service

--- src/api/azamat/Main.java
+++ src/api/azamat/Main.java
@@ -2,6 +2,8 @@ package api.azamat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        WeatherService service = new WeatherService();
+        Weather weather = service.getWeather("Bishkek");
+        weather.print();
     }
 }
