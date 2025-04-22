c5cc34f77299668094662f991f231bcf05e27cfd
Paizidinuulu <Paizidinuulukg@gmail.com>
2025-04-22 18:05:06 +0600

    #JAVA1B-31: Local API - Paizidinuulu

--- src/api/azhibai/CurrencyConverter.java
+++ src/api/azhibai/CurrencyConverter.java
@@ -5,22 +5,19 @@ import java.util.Scanner;
 public class CurrencyConverter {
     public static void main(String[] args) {
 
-        // Валюта курстары
-        double usdRate = 0.0115;  // 1 сом = 0.0115 USD
-        double eurRate = 0.0107;  // 1 сом = 0.0107 EUR
-        double kgsRate = 1;       // 1 сом = 1 KGS (өзү)
+        double usdRate = 0.0115;
+        double eurRate = 0.0107;
+        double kgsRate = 1;
 
-        // Колдонуучу үчүн маалыматтарды алуу
+
         Scanner scanner = new Scanner(System.in);
 
-        // Колдонуучу сумма жана валюта тандоону сурайт
         System.out.println("Конвертациялоо үчүн суммаңызды киргизиңиз (сом):");
         double amount = scanner.nextDouble();
 
         System.out.println("Каалаган валютаны тандаңыз (USD, EUR, KGS):");
         String currency = scanner.next().toUpperCase();
 
-        // Конвертациялоо
         double result = 0;
         if (currency.equals("USD")) {
             result = amount * usdRate;
