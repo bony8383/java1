fdd3ed46f5a68e3f2383d7c0d325b78df617867a
Bachaiy <shayloobekova16@gmail.com>
2025-04-22 17:15:21 +0200

    #43 MethodsExample
    Signed-off-by: Bachaiy <shayloobekova16@gmail.com>
    git pull
    Signed-off-by: Bachaiy <shayloobekova16@gmail.com>

---
+++ src/api/bachaiy/Country.java
@@ -0,0 +1,11 @@
+package api.bachaiy;
+
+public class Country {
+    String name;
+    String population;
+    String languages;
+    String double area;
+    String capital;
+    String President;
+    String  currency;
+}
