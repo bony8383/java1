08eb9df6c6b7077d198665758d49afe9d28fc3ce
Yryskeldi <Yrys>
2025-04-23 13:12:53 +0600

    JAVA2A-16:Create new class helloworld

---
+++ src/helloworld/yryskeldi/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld.yryskeldi;
+
+public class HelloWorld {
+    public  static  void main (String[] args) {
+        String name = "Салам World!!!";
+        String text = "\nМен келдим,\nМен ";
+        Number age = 19;
+        System.out.println(name + text + age +" жаштамын.");
+    }
+}
