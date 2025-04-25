50ee7d73a7c02f2c69815c7d60df4fd67cea81ab
Musurmankul Saidov <saidovmusurmankul@gmail.com>
2025-04-25 10:04:59 +0600

    #JAVA2B-27: HelloWorld resolved

--- src/helloworld/muslim/HelloWorld.java
+++ src/helloworld/muslim/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld.muslim;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Салам Дүйнө");
+        System.out.println("Салам Кыргызстан");
     }
 }
