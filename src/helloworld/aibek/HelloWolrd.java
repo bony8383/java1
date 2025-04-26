7212f1fae837899c71c8918a7f2bbda9cf3122ce
Aibek <arstanbekov77@gmail.com>
2025-04-26 08:58:57 +0200

    JAVA2A-12: Create first  Hello World Java class

---
+++ src/helloworld/aibek/HelloWolrd.java
@@ -0,0 +1,9 @@
+package helloworld.aibek;
+
+public class HelloWolrd {
+    public static void main (String[] args){
+        String name = "Hello World";
+        System.out.println(name) ;
+    }
+
+}
