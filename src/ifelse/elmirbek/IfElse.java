a7dc0e23e03e8485b349ac211e621a6242d7d381
твое_имя <e1mirbek>
2025-05-04 02:12:29 +0600

    #JAVA@B-56-Elmirbek : Add weather-based clothing recommendation based on user input temperature

--- src/ifelse/elmirbek/IfElse.java
+++ src/ifelse/elmirbek/IfElse.java
@@ -1,4 +1,27 @@
 package ifelse.elmirbek;
 
-public class IfElse {
+import java.util.Scanner;
+public  class IfElse  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Аба ырайына жараша билдируу !!! ");
+
+        // сразу начнем с проверки чтобы программа работало корректно с числами а не строкой !!!
+
+        // 1 первая проверка введено число !!!
+        if (scanner.hasNextInt()) {
+            int temperature = scanner.nextInt();
+            if (temperature < 5) {
+                System.out.println("Аба суук. Жылуу кийим кииңиз. . Темпратура : " + temperature + " градуска суук !");
+            } else if (temperature <= 15) {
+                System.out.println("Кадимки кийим кииңиз. Темпратруа : " + temperature + " градуска жылуу !");
+            } else {
+                System.out.println("Аба ыссык. Женил кийим кииңиз. Темпратура :  " + temperature + "градуска ыссык" );
+            }
+        }
+        else  {
+            System.out.println("КАТААА! Сан жазыңыз, текст эмес!");
+            scanner.close();
+        }
+    }
 }
