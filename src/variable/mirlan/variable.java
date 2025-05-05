035ceee0d82d2c84e3e19d066234ecdbff36e5c6
mirlanneman <mirlannarynbekuulu@gmail.com>
2025-05-05 21:32:37 +0600

    #JAVA2B-29: Create my first class HelloWorld after long week

--- src/variable/mirlan/variable.java
+++ src/variable/mirlan/variable.java
@@ -3,7 +3,7 @@ package variable.mirlan;
 import java.util.Scanner;
 
 public class variable {
-    public static <scunner> void main(String[] args) {
+    public static  void main(String[] args) {
 
         // Озгормолорду тузуу
         String name = "Mirlan Narynbek Uulu";
