36c06ef8ed2eefaec28f5aa0a4009229b746fb33
Aibek <arstanbekov77@gmail.com>
2025-05-09 23:20:51 +0200

    JAVA2A-12: ForWhile.java ,MultiplicationTable,ForWhileExample

---
+++ src/methods/aibek/MethodsExample.java
@@ -0,0 +1,12 @@
+package methods.aibek;
+
+public class MethodsExample {
+    public static void main(String[] args) {
+        //методду чакыруу.
+        greetUser("Aibek");
+    }
+    static void greetUser(String name){
+        System.out.println("Salam "+  name + " ! ") ;
+    }
+    //Параметрсиз жана кайтаруусуз метод
+}
