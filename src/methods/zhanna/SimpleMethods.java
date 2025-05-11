20409c7f58e3f86d88bcb28023bf8bc34e05f14b
Zhanyl Kubatbek kyzy <kubatbekova.janyl98@gmail.com>
2025-05-11 22:08:16 +0200

    added methods

---
+++ src/methods/zhanna/SimpleMethods.java
@@ -0,0 +1,13 @@
+package methods.zhanna;
+
+public class SimpleMethods {
+    // Метод: колдонуучунун атына ылайык салам айтат
+    public static void greetUser(String name) {
+        System.out.println("Салам, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        // Методду чакыруу
+        greetUser("Ainura");
+    }
+}
